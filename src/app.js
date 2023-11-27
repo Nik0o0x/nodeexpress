940ae64f19133d2e6f42fba8dd812d5cfdfb727c
Nikodem Redmann <nikodem.redmann@wp.pl>
2023-11-27 09:28:59 +0100

    teraz wszystko w porzo

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ hbs.registerPartials(path.join(__dirname,'views/partials'));
 app.set('view engine' , 'hbs');
 
 app.get('/', (req, res) => {
-//res.send("Strona działa!")//
+
 res.render('index' , {});
 });
 
